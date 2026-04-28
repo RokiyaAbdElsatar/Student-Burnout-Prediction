81f6472acecdd450222519603cde07e744854bfb
ANIS Developer <dev@anis.com>
2026-04-29 00:41:45 +0300

    Add requirements.txt and fix start_gui.sh

--- Python/Student-Burn-out-Prediction/start_gui.sh
+++ Python/Student-Burn-out-Prediction/start_gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the Streamlit GUI for Student Burnout Prediction
 
-cd "/mnt/60F20F1EF20EF854/projects/Python/Student-Burnout-Prediction"
+cd "$(dirname "$0")"
 source venv/bin/activate
 streamlit run gui/app.py
